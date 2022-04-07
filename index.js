bb3b86cbe87bb0ef342e873c81602778a3ee2a14
Aris Vayas <aris.vayas@gmail.com>
2022-04-07 09:36:29 -0400

    commit tree bd0fc018fe6e7df2880b15b389022cf77661f941

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
+   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
+   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
+   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// code your solution here
+
+let newArray = []
+const getTitleCase = tutorials.map(function (sentence){
+  let titleCase = sentence.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  return newArray.push(titleCase)
+})
+function titleCased(){
+  return newArray
 }
